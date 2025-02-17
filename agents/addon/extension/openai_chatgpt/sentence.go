9a9ef6b9a8748148e772c6f5da5f9a64d70231cb
zhengang.yu <kingchdu@gmail.com>
2025-02-17 19:45:01 +0800

    update sentence judge loginc

--- agents/addon/extension/openai_chatgpt/sentence.go
+++ agents/addon/extension/openai_chatgpt/sentence.go
@@ -13,17 +13,25 @@ func isPunctuation(r rune) bool {
 func parseSentence(sentence, content string) (string, string, bool) {
 	var remain string
 	var foundPunc bool
+	var lastChar rune
 
 	for _, r := range content {
+		// To avoid too many breaks due to consecutive punctuation
+        if isPunctuation(r) && isPunctuation(lastChar) {
+            sentence += string(r)
+            continue
+        }
 		if !foundPunc {
 			sentence += string(r)
 		} else {
 			remain += string(r)
 		}
 
-		if !foundPunc && isPunctuation(r) {
+		if !foundPunc && isPunctuation(r) && len(sentence) > 3 {
 			foundPunc = true
 		}
+
+		lastChar = r
 	}
 
 	return sentence, remain, foundPunc
